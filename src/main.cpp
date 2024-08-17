fc375d3cd8095b41239d37ec2dd99ed93266651f
sebidev <sebidev@hotmail.com>
2024-08-17 22:54:00 +0200

    commit two

--- src/main.cpp
+++ src/main.cpp
@@ -13,28 +13,30 @@ unsigned long lastMsg = 0;
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
 
-// Variables for each sensor
 // Tuya Sensor 1
-String ts1_battery_state;
-float ts1_humidity;
-float ts1_temperature;
-unsigned long lastTS1Msg = 0; // Time of the last received message
+String ts1_battery_state = ""; 
+float ts1_humidity = NAN;       
+float ts1_temperature = NAN;    
+unsigned long lastTS1Msg = 0;
 
 // Tuya Sensor 2
-String ts2_battery_state;
-float ts2_humidity;
-float ts2_temperature;
-unsigned long lastTS2Msg = 0; // Time of the last received message
+String ts2_battery_state = "";
+float ts2_humidity = NAN;
+float ts2_temperature = NAN;
+unsigned long lastTS2Msg = 0;
 
 // Tuya Sensor 3
-String ts3_battery_state;
-float ts3_humidity;
-float ts3_temperature;
-unsigned long lastTS3Msg = 0; // Time of the last received message
+String ts3_battery_state = "";
+float ts3_humidity = NAN;
+float ts3_temperature = NAN;
+unsigned long lastTS3Msg = 0;
 
-// Timeout interval in milliseconds (e.g., 10 seconds)
-const unsigned long timeoutInterval = 10000;
-
+bool areAllSensorsUninitialized() {
+  bool ts1_uninitialized = ts1_battery_state.isEmpty() && isnan(ts1_humidity) && isnan(ts1_temperature);
+  bool ts2_uninitialized = ts2_battery_state.isEmpty() && isnan(ts2_humidity) && isnan(ts2_temperature);
+  bool ts3_uninitialized = ts3_battery_state.isEmpty() && isnan(ts3_humidity) && isnan(ts3_temperature);
+  return ts1_uninitialized && ts2_uninitialized && ts3_uninitialized;
+}
 
 void setup_wifi() {
   delay(10);
@@ -64,11 +66,15 @@ void reconnect() {
 
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    String clientId = "ESP8266Client-";
+    String clientId = "ESP32Client-";
     clientId += String(random(0xffff), HEX);
     
-    if (client.connect(clientId.c_str())) {
+    if (client.connect(clientId.c_str(), mqtt_user, mqtt_userpassword)) {
       Serial.println("connected");
+      client.subscribe(ts1_topic);
+      client.subscribe(ts2_topic);
+      client.subscribe(ts3_topic);
+      client.publish("ESP32Topic", clientId.c_str());
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -95,25 +101,25 @@ void callback(char* topic, byte* payload, unsigned int length) {
     return;
   }
 
-  unsigned long now = millis(); // Current time
+  unsigned long now = millis();
   
   if (strcmp(topic, ts1_topic) == 0) {
     ts1_battery_state = doc["battery_state"].as<String>();
     ts1_humidity = doc["humidity"];
     ts1_temperature = doc["temperature"];
-    lastTS1Msg = now; // Update last message time
+    lastTS1Msg = now;
   } 
   else if (strcmp(topic, ts2_topic) == 0) {
     ts2_battery_state = doc["battery_state"].as<String>();
     ts2_humidity = doc["humidity"];
     ts2_temperature = doc["temperature"];
-    lastTS2Msg = now; // Update last message time
+    lastTS2Msg = now;
   } 
   else if (strcmp(topic, ts3_topic) == 0) {
     ts3_battery_state = doc["battery_state"].as<String>();
     ts3_humidity = doc["humidity"];
     ts3_temperature = doc["temperature"];
-    lastTS3Msg = now; // Update last message time
+    lastTS3Msg = now;
   }
 }
 
@@ -135,14 +141,28 @@ void setup(){
     pinMode(GFX_BL, OUTPUT);
     digitalWrite(GFX_BL, HIGH);
   #endif
-
+/* 
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+ */
   show_sensorvalue("Outdoor", 20.3, 78, "medium");
 }
 
 
 void loop(){
-/* show_noWifi();
-  delay(15000); // 15 seconds
-  show_waitsync();
-  delay(15000); // 15 seconds */
+/*   
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  if (areAllSensorsUninitialized()) {
+    Serial.println("Alle Sensorwerte sind uninitialisiert oder leer.");
+    show_waitsync();
+  } else {
+    Serial.println("Mindestens ein Sensor hat gültige Werte.");
+    show_sensorvalue("Outdoor", ts1_temperature, ts1_humidity, ts1_battery_state);
+  }
+ */
 }
